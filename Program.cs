e996ddbcdd65498f321451e051ed866782f0a996
Ethan Jaworski <edjaworsk@gmail.com>
2023-02-12 23:12:35 -0600

    Final Commit

--- Program.cs
+++ Program.cs
@@ -3,21 +3,30 @@
 // ask for input
 // create data file
 
-    // ask a question
-    Console.WriteLine("How many weeks of data is needed?");
-    // input the response (convert to int)
-    int weeks = int.Parse(Console.ReadLine());
-     // determine start and end date
-    DateTime today = DateTime.Now;
-    // we want full weeks sunday - saturday
-    DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
-    // subtract # of weeks from endDate to get startDate
-    DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
-    Console.WriteLine(dataDate);
-    // random number generator
-    Random rnd = new Random();
-    // create file
-    StreamWriter sw = new StreamWriter("data.txt");
+// ask a question
+Console.WriteLine("How many weeks of data is needed?");
+// input the response (convert to int)
+int weeks = int.Parse(Console.ReadLine());
+    // determine start and end date
+DateTime today = DateTime.Now;
+// we want full weeks sunday - saturday
+DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+// subtract # of weeks from endDate to get startDate
+DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+Console.WriteLine(dataDate);
+// random number generator
+Random rnd = new Random();
+// create file
+StreamWriter sw = new StreamWriter("data.txt");
+
+// 7 days in a week
+int[] hours = new int[7];
+
+for (int i = 0; i < hours.Length; i++)
+    {
+        // generate random number of hours slept between 4-12 (inclusive)
+        hours[i] = rnd.Next(4, 13);
+    }
 
 Console.WriteLine("Enter 1 to create data file.");
 Console.WriteLine("Enter 2 to parse data.");
@@ -30,13 +39,6 @@ if (resp == "1")
     // loop for the desired # of weeks
     while (dataDate < dataEndDate)
     {
-        // 7 days in a week
-        int[] hours = new int[7];
-        for (int i = 0; i < hours.Length; i++)
-        {
-            // generate random number of hours slept between 4-12 (inclusive)
-            hours[i] = rnd.Next(4, 13);
-        }
         // M/d/yyyy,#|#|#|#|#|#|#
         Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
         // Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
@@ -52,9 +54,10 @@ else if (resp == "2")
     while (dataDate < dataEndDate)
     {
         Console.WriteLine($"Week of {today:MMM}, {today:dd}, {today:yyyy}");
-        // It would be more work to use string interpolation here.
         Console.WriteLine("Su Mo Tu We Th Fr Sa");
         Console.WriteLine("-- -- -- -- -- -- --");
+        Console.WriteLine($"{hours[0]} {hours[1]} {hours[2]} {hours[3]} {hours[4]} {hours[5]} {hours[6]} ");
+        Console.WriteLine();
         dataDate = dataDate.AddDays(7);
         today = today.AddDays(7);
     }
